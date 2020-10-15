d62d21f9a2fb3631e158ceb79a8795abb438fd74
shuichi <shu.heihei1209@gmail.com>
2020-10-15 11:43:33 +0900

    javaでの計算

---
+++ Calc1.java
@@ -0,0 +1,31 @@
+public class Calc1 {
+	public static void main(String[] args) {
+		 System.out.println("加算の結果は"+ (3+2) +"です。");
+	     System.out.println("減算の結果は"+ (3-2) +"です。");
+		 System.out.println("乗算の結果は"+ (3*2) +"です。");
+		 System.out.println("除算の結果は"+ (7/3) +"です。");
+		 System.out.println("除算の結果は"+ ((1+2)*3-4/2) +"です。");
+		 System.out.println("除算の結果は"+ (2-3*4) +"です。");
+		 System.out.println("加算の練習"+3+2+"です。");
+		 System.out.println("除算の結果は"+ (10/100) +"です。");
+         
+         //練習問題
+         System.out.println( "0×0="+(0*0));
+         System.out.println( "1×1="+(1*1));
+         System.out.println( "2×2="+(2*2));
+         System.out.println( "3×3="+(3*3));
+         System.out.println( "4×4="+(4*4));
+         System.out.println( "5×5="+(5*5));
+         System.out.println( "6×6="+(6*6));
+         System.out.println( "7×7="+(7*7));
+         System.out.println( "8×8="+(8*8));
+         System.out.println( "9×9="+(9*9));
+         System.out.println( "10×10="+(10*10));
+
+         //面積の計算 面積=(上底+下底)×高さ÷2
+          System.out.println("面積="+(2+3)*4/2);
+
+          //Lをつけることでlong型になる
+          System.out.println(100000L*100000L);
+	}
+}
