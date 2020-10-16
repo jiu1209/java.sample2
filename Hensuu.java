1a9eb22354a2dfda0dfc91376afd3e9b3c99ce35
shuichi <shu.heihei1209@gmail.com>
2020-10-16 17:39:07 +0900

    javaの変数

---
+++ Hensuu.java
@@ -0,0 +1,20 @@
+public class Hensuu {
+	public static void main(String[] args) {
+		int x = 80;
+        int c = 40;
+		System.out.println(x/c);
+
+        //小数点
+		double b = 15.0;
+		double n = 32.0;
+		System.out.println((b+n) / 2);
+
+		//文字列
+		char a = 'A';
+		char alpha = 'a';
+		char syuichi = '修';
+		System.out.println("変数aの値は" + a);
+		System.out.println("変数alphaの値は" + alpha);
+		System.out.println("変数syuichiの値は" + syuichi);
+	}
+}
