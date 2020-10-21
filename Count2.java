6f4a51f04b633b52b703a84c93131cce1996dc80
shuichi <shu.heihei1209@gmail.com>
2020-10-21 12:10:24 +0900

    for文

---
+++ Count2.java
@@ -0,0 +1,11 @@
+public class Count2 {
+	public static void main(String[] args) {
+		int i;
+		for (i = 0; i < 10; i++) {
+			System.out.print(i + "の2乗は"+(i*i)+"で、");
+			System.out.println("3乗は"+(i*i*i)+"です。");
+		}
+		System.out.println(i);
+		System.out.println("end");
+	}
+}
